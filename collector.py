ebaed15329aeb186f72eb21bc5a5ad52f0dedb72
ds1sqe <ds1sqe@mensakorea.org>
2024-07-11 07:24:22 +0900

    edit collector

--- collector.py
+++ collector.py
@@ -1,30 +1,36 @@
 # Collect data and save to JSON
+print("importing libs")
 
 import os, time, random, datetime
 import pathlib, requests, json
 
+from dateutil import parser
 from dotenv import load_dotenv
 
+print("loading dotenv")
 # Load environment variables
 load_dotenv()
 
+print("define consts")
 BASE_DIR = pathlib.Path(os.path.abspath("")).resolve()
 GITHUB_API_URL = "https://api.github.com"
-GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 GITHUB_API_KEY = os.getenv("GITHUB_API_KEY")
+GITHUB_API_KEY2 = os.getenv("GITHUB_API_KEY2")
+
+keys = [GITHUB_API_KEY,GITHUB_API_KEY2]
+key_idx = 0
 
 repo_checked = set();
 
-headers = {
-    "Authorization": f"token {GITHUB_TOKEN}",
-    "Accept": "application/vnd.github.v3+json"
-}
+for file in os.listdir(BASE_DIR / "data" ):
+    repo_checked.add(file[:-6])
 
 def get_random_repository(query):
     """
     Get random repository object from GitHub.
 
     """
+    global key_idx
 
     # Backoff setting to prevent rate limit
     # backoff = (backoff * coeff_m) + (random * coeff_r)
@@ -34,10 +40,12 @@ def get_random_repository(query):
 
     # Set up the GitHub API endpoint for searching repositories
     api_url = "https://api.github.com/search/repositories"
-    headers = {
+    header = {
         "Accept": "application/vnd.github+json",
-        "Authorization": f"Bearer {GITHUB_API_KEY}",
+        "Authorization": f"Bearer {keys[key_idx]}",
     }
+
+    print("header: ",header)
 
     # Set up the parameters for the search query
     while True:
@@ -46,7 +54,7 @@ def get_random_repository(query):
             "page": random.randint(1, 30),
         }
 
-        response = requests.get(api_url, params=params, headers=headers)
+        response = requests.get(api_url, params=params, headers=header)
 
         if response.status_code == 200:
             data = response.json()
@@ -56,24 +64,15 @@ def get_random_repository(query):
                 return random_repo
 
             else:
-                continue
+                key_idx = 0 if key_idx == 1 else 1
+                backoff = backoff * coeff_m + random.random() * coeff_r
+                time.sleep(backoff)
 
-        backoff = backoff * coeff_m + random.random() * coeff_r
-        time.sleep(backoff)
 
 def get_data(repo):
-    keys = [
-        "full_name",
-        "html_url",
-        "size",
-        "stargazers_count",
-        "language",
-        "forks_count",
-        "topics",
-    ]
-
-    repo_data = {key: repo.get(key) for key in keys}
+    global key_idx
     issue_url = repo.get("issues_url").rstrip("{/number}")
+    name= repo.get("name")
 
     page = 1
 
@@ -83,77 +82,49 @@ def get_data(repo):
             params={"state": "closed", "page": page},
             headers={
                 "Accept": "application/vnd.github+json",
-                "Authorization": f"Bearer {GITHUB_API_KEY}",
+                "Authorization": f"Bearer {keys[key_idx]}",
             },
         )
 
         if response.status_code == 200:
             issues = response.json()
-
         else:
+            print("\nResponse code is not 200")
+            print("Response: ",response.text)
+            time.sleep(60)
+            key_idx = 0 if key_idx == 1 else 1
             continue
 
         if len(issues) == 0:
+            print("There is no issue\n")
             break
 
         for issue in issues:
-            issue_type = "pull_request" if "pull_request" in issue else "issue"
-
-            created_at = datetime.fromisoformat(issue.get("created_at").rstrip("Z"))
-            closed_at = datetime.fromisoformat(issue.get("closed_at").rstrip("Z"))
-            commented_at = closed_at
-
-            response = requests.get(
-                issue.get("timeline_url"),
-                params={"per_page": 100, "page": 1},
-                headers={
-                    "Accept": "application/vnd.github+json",
-                    "Authorization": f"Bearer {GITHUB_API_KEY}",
-                },
-            )
-
-            if response.status_code == 200 and (timelines := response.json()):
-                for timeline in timelines:
-                    match timeline.get("event"):
-                        case "reviewed":
-                            time = timeline.get("submitted_at").rstrip("Z")
-
-                        case "commented":
-                            time = timeline.get("created_at").rstrip("Z")
-
-                        case _:
-                            continue
-
-                    commented_at = datetime.fromisoformat(time)
-                    break
-
-            lifetime = closed_at - created_at
-            worktime = commented_at - created_at
-
-            #                   sec  min  hour
-            SECONDS_IN_DAYS = ( 60 * 60 * 24 )
-
-            lifetime_day: float = lifetime.total_seconds() / SECONDS_IN_DAYS
-            worktime_day: float = worktime.total_seconds() / SECONDS_IN_DAYS
-
-            with open(BASE_DIR / "results.jsonl", "a") as f:
-                json.dump(
-                    {
-                        "repository": repo_data,
-                        "issue_type": issue_type,
-                        "closed_at": str(closed_at),
-                        "created_at": str(created_at),
-
-                        "lifetime": str(lifetime),
-                        "worktime": str(worktime),
-
-                        "lifetime_day": str(lifetime_day),
-                        "worktime_day": str(worktime_day),
+            title = issue.get("title")
+            print(f"{name} <{page}> title:{title}\n")
+            while True:
+                timeline_response = requests.get(
+                    issue.get("timeline_url"),
+                    # params={"per_page": 100, "page": 1},
+                    headers={
+                        "Accept": "application/vnd.github+json",
+                        "Authorization": f"Bearer {keys[key_idx]}",
                     },
-                    f,
                 )
-                f.write("\n")
+                if timeline_response.status_code == 200:
+                    with open(BASE_DIR / f"data/{name}.jsonl", "a") as f:
+                        issue.update({"time_line": timeline_response.json()})
+                        json.dump(issue,f)
+                        f.write("\n")
+                        break
+                else:
+                    print("\nResponse code is not 200 (timeline)")
+                    print("Response: ",response.text)
+                    time.sleep(60)
+                    key_idx = 0 if key_idx == 1 else 1
 
+        if page > 300:
+            break
         page += 1
 
 def collect():
@@ -161,7 +132,7 @@ def collect():
         try:
             repo = get_random_repository(query="is:public")
 
-            if (repo_name := repo.get("full_name")) in repo_checked:
+            if (repo_name := repo.get("name")) in repo_checked:
                 continue
 
             repo_checked.add(repo_name)
@@ -171,3 +142,10 @@ def collect():
 
         except Exception as e:
             print(e)
+
+def main():
+    print("main called")
+    collect()
+
+if __name__ == '__main__':
+    main()
